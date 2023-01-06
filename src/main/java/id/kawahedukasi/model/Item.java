7deb62ed6de2673424c212348ff66ef667905e79
Ananda Dana Pratama <ananda.pratama@ifg-life.id>
2023-01-06 20:56:42 +0700

    init

---
+++ src/main/java/id/kawahedukasi/model/Item.java
@@ -0,0 +1,97 @@
+package id.kawahedukasi.model;
+
+import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "item")
+public class Item extends PanacheEntityBase {
+    @Id
+    @GenericGenerator(name = "uuid", strategy = "org.hibernate.id.UUIDGenerator")
+    @GeneratedValue(generator = "uuid")
+    @Column(name = "id", nullable = false, length = 36)
+    private String id; //uuid
+
+    @Column(name = "name", nullable = false, length = 50)
+    private String name;
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "count", nullable = false)
+    private Long count;
+
+    @Column(name = "type", nullable = false, length = 20)
+    private String type;
+
+    @Column(name = "description", columnDefinition = "text")
+    private String description;
+
+    @CreationTimestamp
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    public Item() {
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public Long getCount() {
+        return count;
+    }
+
+    public void setCount(Long count) {
+        this.count = count;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+}
---
+++ src/main/java/id/kawahedukasi/model/ResponseCustom.java
@@ -0,0 +1,7 @@
+package id.kawahedukasi.model;
+
+public class ResponseCustom {
+    public String message;
+    public Integer statusCode;
+    public Object data;
+}
